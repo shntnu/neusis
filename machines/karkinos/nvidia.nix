c31fce585d19969b800470f1ea51572a2cacf84b
Ankur Kumar <ank@leoank.me>
2024-02-14 14:33:14 -0500

    Fix(module): Confusion between options and config

--- machines/karkinos/nvidia.nix
+++ machines/karkinos/nvidia.nix
@@ -4,30 +4,31 @@ let
 in
 {
 
-  hardware = {
+  options = { 
+    hardware = {
+      # Enable OpenGL
+      opengl = {
+        enable = true;
+        driSupport = true;
+        driSupport32Bit = true;
+        extraPackages = with pkgs; [
+          vaapiVdpau
+        ];
+      };
 
-    # Enable OpenGL
-    opengl = {
-      enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
-      extraPackages = with pkgs; [
-        vaapiVdpau
-      ];
+      # Configure Nvidia driver
+      nvidia = {
+        modesetting.enable = true;
+        powerManagement.enable = false;
+        open = false;
+        nvidiaSettings = true;
+        package = package_ver;
+      };
     };
-
-    # Configure Nvidia driver
-    nvidia = {
-      modesetting.enable = true;
-      powerManagement.enable = false;
-      open = false;
-      nvidiaSettings = true;
-      package = package_ver;
-    };
+    
+    # Nvidia and Cuda support
+    services.xserver.videoDrivers = ["nvidia"];
   };
-  
-  # Nvidia and Cuda support
-  services.xserver.videoDrivers = ["nvidia"];
   config = {
     allowUnfree = true;
     cudaSupport = true;
