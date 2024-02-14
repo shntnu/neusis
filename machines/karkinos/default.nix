1c3ff0384fc3299c766d2dbc941f11a60e49eb2e
Ankur Kumar <ank@leoank.me>
2024-02-14 14:39:57 -0500

    Chore(cleanup): Remove options not needed for now

--- machines/karkinos/default.nix
+++ machines/karkinos/default.nix
@@ -25,25 +25,25 @@
     ./secrets.nix
   ];
 
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    # Configure your nixpkgs instance
-    config = {
-      # Disable if you don't want unfree packages
-      allowUnfree = true;
-    };
-  };
+  # nixpkgs = {
+  #   # You can add overlays here
+  #   overlays = [
+  #     # If you want to use overlays exported from other flakes:
+  #     # neovim-nightly-overlay.overlays.default
+  #
+  #     # Or define it inline, for example:
+  #     # (final: prev: {
+  #     #   hi = final.hello.overrideAttrs (oldAttrs: {
+  #     #     patches = [ ./change-hello-to-hi.patch ];
+  #     #   });
+  #     # })
+  #   ];
+  #   # Configure your nixpkgs instance
+  #   config = {
+  #     # Disable if you don't want unfree packages
+  #     allowUnfree = true;
+  #   };
+  # };
 
   # This will add each flake input as a registry
   # To make nix3 commands consistent with your flake
@@ -102,10 +102,10 @@
   services.xserver.desktopManager.gnome.enable = false;
 
   # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
+  # services.xserver = {
+  #   layout = "us";
+  #   xkbVariant = "";
+  # };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
@@ -116,7 +116,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.ank = {
     isNormalUser = true;
-    passwordFile = config.age.secrets.karkinos_pass.path;
+    # passwordFile = config.age.secrets.karkinos_pass.path;
     description = "Ankur Kumar";
     extraGroups = [ "networkmanager" "wheel" "libvirt" ];
     openssh.authorizedKeys.keyFiles = [ ../../homes/ank/id_rsa.pub ];
