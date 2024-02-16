603126aa79d91531311f98bd9d02ddf9a0fd08c5
Ankur Kumar <ank@leoank.me>
2024-02-15 21:02:52 -0500

    Fix(hm module:gui): Move it to machine config

--- machines/karkinos/default.nix
+++ machines/karkinos/default.nix
@@ -32,6 +32,14 @@
     ../us_eng.nix
   ];
 
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = false;
+
   # nixpkgs = {
   #   # You can add overlays here
   #   overlays = [
