bd7b043a36a6485f66ec5b4295d423d4b6b007a1
Ankur Kumar <ank@leoank.me>
2024-02-16 17:31:41 -0500

    Feat(secrets): Add karkinos as a recipient

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,8 @@
 let
   ank = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINp8DUNWx6rrzqYU8ejdQxxbXpS/rmp+G/3HXDozwNu6 ank@leoank.me";
+  karkinos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINp8DUNWx6rrzqYU8ejdQxxbXpS/rmp+G/3HXDozwNu6 ank@leoank.me";
 in
 {
   "ank_userpass.age".publicKeys = [ ank ];
-  "tsauthkey.age".publicKeys = [ ank ];
+  "tsauthkey.age".publicKeys = [ ank karkinos ];
 }
