52c3173f8b1db7afe64bfd185d9f21b93e6431f9
Ankur Kumar <ank@leoank.me>
2024-02-20 17:35:47 -0500

    Fix(hmm:git): Add gpg signers key path

--- homes/ank/dev/git.nix
+++ homes/ank/dev/git.nix
@@ -10,6 +10,7 @@
         # Sign all commits using ssh key
         commit.gpgsign = true;
         gpg.format = "ssh";
+        gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
         user.signingkey = "~/.ssh/id_ed25519.pub";
       };
   };
