7c110ed4ddeb48b58378368b4b64575c1eca61a4
Ankur Kumar <ank@leoank.me>
2024-02-20 17:08:57 -0500

    Feat(hmm:git): Add code signing for git

--- homes/ank/dev/git.nix
+++ homes/ank/dev/git.nix
@@ -1,8 +1,16 @@
-{ ... }:
+{ config, ... }:
 {
+  home.file.".ssh/allowed_signers".text =
+    "* ${builtins.readFile /home/${config.home.username}/.ssh/id_ed25519.pub}";
   programs.git = {
     enable = true;
     userName = "Ankur Kumar";
     userEmail = "ank@leoank.me";
+    extraConfig = {
+        # Sign all commits using ssh key
+        commit.gpgsign = true;
+        gpg.format = "ssh";
+        user.signingkey = "~/.ssh/id_ed25519.pub";
+      };
   };
 }
