da4c36532c47dc94fbcce6e134ed584c171cc681
Ankur Kumar <ank@leoank.me>
2024-06-26 18:04:55 -0400

    Fix(nm: disko): Update hostID

--- machines/karkinos/default.nix
+++ machines/karkinos/default.nix
@@ -111,7 +111,7 @@
   # Netowrking
   networking.hostName = "GPFDA-11A";
 
-  networking.hostId = "karkinos"; # The primary use case is to ensure when using ZFS that a pool isn’t imported accidentally on a wrong machine.
+  networking.hostId = "df6b910c"; # The primary use case is to ensure when using ZFS that a pool isn’t imported accidentally on a wrong machine.
   # networking.bridges.br0.interfaces = [ "enp2s0" "wlp131s0" ];
   services.tailscale.enable = true;
 
