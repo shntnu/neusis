900f400d415590f19c6d8ba3c7e958fc98f05ca6
Ankur Kumar <ank@leoank.me>
2025-01-09 06:07:31 -0500

    update(templates): add init project to pythonml

--- templates/pythonml/flake.nix
+++ templates/pythonml/flake.nix
@@ -42,7 +42,7 @@
               cudatoolkit
 
               # This is required for most app that uses graphics api
-              linuxPackages.nvidia_x11
+              # linuxPackages.nvidia_x11
             ]
           );
       in
@@ -65,6 +65,17 @@
                 python311Packages.venvShellHook
                 # We # We now recommend to use uv for package management inside nix env
                 uv
+
+                # Data sharing tools
+                # syncthing
+                # jq
+
+                # Data inspections tools
+                # duckdb
+                # mongodb
+
+                # video tools
+                ffmpeg
               ] ++ libList;
               venvDir = "./.venv";
               postVenvCreation = ''
@@ -90,3 +101,9 @@
 # export LD_LIBRARY_PATH=${pkgs.cudaPackages.cuda_nvrtc}/lib
 # export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
 # export EXTRA_CCFLAGS="-I/usr/include"
+
+# Data syncthing commands
+# syncthing cli show system | jq .myID
+# syncthing cli config devices add --device-id $DEVICE_ID_B
+# syncthing cli config folders $FOLDER_ID devices add --device-id $DEVICE_ID_B
+# syncthing cli config devices $DEVICE_ID_A auto-accept-folders set true
