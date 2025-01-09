244c11fb6044a169d9d5ed8b095b183529db65f8
Ankur Kumar <ank@leoank.me>
2025-01-09 03:50:54 -0500

    update(templates): pythonml

--- templates/pythonml/flake.nix
+++ templates/pythonml/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs_master.url = "github:NixOS/nixpkgs/master";
     systems.url = "github:nix-systems/default";
     flake-utils.url = "github:numtide/flake-utils";
@@ -37,17 +37,12 @@
             pkgs.glib
           ]
           ++ pkgs.lib.optionals pkgs.stdenv.isLinux (
-            with mpkgs.cudaPackages;
+            with pkgs;
             [
-              libcublas
-              libcurand
-              cudnn
-              libcufft
-              cuda_cudart
-              cuda_nvrtc
+              cudatoolkit
 
               # This is required for most app that uses graphics api
-              pkgs.linuxPackages.nvidia_x11
+              linuxPackages.nvidia_x11
             ]
           );
       in
@@ -56,7 +51,7 @@
         devShells = {
           default =
             let
-              python_with_pkgs = pkgs.python310.withPackages (pp: [
+              python_with_pkgs = pkgs.python311.withPackages (pp: [
                 # Add python pkgs here that you need from nix repos
               ]);
             in
@@ -67,7 +62,7 @@
               NIX_LD_LIBRARY_PATH = lib.makeLibraryPath libList;
               packages = [
                 python_with_pkgs
-                python310Packages.venvShellHook
+                python311Packages.venvShellHook
                 # We # We now recommend to use uv for package management inside nix env
                 uv
               ] ++ libList;
