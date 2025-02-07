26563825da78708279f467f7a4896531a294fb1f
Ankur Kumar <ank@leoank.me>
2025-02-06 21:33:59 -0500

    fix(oppy): grub device -> devices

--- machines/oppy/boot.nix
+++ machines/oppy/boot.nix
@@ -1,11 +1,10 @@
 { ... }:
 {
   # Bootloader
-  boot.shell_on_fail = true;
   boot.loader.grub = {
     enable = true;
     efiSupport = true;
     efiInstallAsRemovable = true;
-    device = "/dev/nvme4n1";
+    devices = "/dev/nvme4n1";
   };
 }
