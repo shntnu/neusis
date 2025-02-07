dd52eef4716adff303b98622738cbc203ebe500a
Ankur Kumar <ank@leoank.me>
2025-02-06 21:08:28 -0500

    fix(oppy): grub device issue

--- machines/oppy/boot.nix
+++ machines/oppy/boot.nix
@@ -6,11 +6,6 @@
     zfsSupport = true;
     efiSupport = true;
     efiInstallAsRemovable = true;
-    mirroredBoots = [
-      {
-        devices = [ "/dev/nvme4n1" ];
-        path = "/boot";
-      }
-    ];
+    device = "/dev/nvme4n1";
   };
 }
