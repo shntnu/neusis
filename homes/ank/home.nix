aab63b312fbe3132c98a251be278c8f934f41610
Ankur Kumar <ank@leoank.me>
2025-04-09 13:00:40 -0400

    chore(hm: packages): add nix tools to ank home

--- homes/ank/home.nix
+++ homes/ank/home.nix
@@ -8,6 +8,21 @@
       duckdb
       jq
       mpv
+      nix-output-monitor
+      nh
+      nixos-shell
+      television
+      comma
+      manix
+      nix-index
+      nix-diff
+      nix-du
+      nix-melt
+      nix-tree
+      nix-init
+      nvd
+      nurl
+      statix
     ];
   };
 }
