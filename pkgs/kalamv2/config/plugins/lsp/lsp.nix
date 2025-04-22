dff0f534418769ad9f2436f8bcc3429297564e94
Ankur Kumar <ank@leoank.me>
2025-04-22 04:29:05 -0400

    fix(kalamv2): lsp keymaps

--- pkgs/kalamv2/config/plugins/lsp/lsp.nix
+++ pkgs/kalamv2/config/plugins/lsp/lsp.nix
@@ -5,8 +5,16 @@
     lsp = {
       enable = true;
       servers.typos_lsp.enable = true;
+      keymaps.lspBuf = {
+        "gd" = "definition";
+        "gD" = "references";
+        "gt" = "type_definition";
+        "gi" = "implementation";
+        "K" = "hover";
+      };
     };
     lint.enable = true;
+
   };
 
 }
