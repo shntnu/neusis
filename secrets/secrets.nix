1c0d0728d850bc966e7ca5a03d346e369ec9cb25
Ankur Kumar <ank@leoank.me>
2025-09-02 22:52:16 -0400

    fix(agenix): secrets management

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,15 @@
 let
   ank = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINp8DUNWx6rrzqYU8ejdQxxbXpS/rmp+G/3HXDozwNu6 ank@leoank.me";
+  users = [ ank ];
+
   karkinos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFH40XzfXPtcTwJ8FHxHXCaEteylFOwtuw5TaY5CZ5NS ank@leoank.me";
-in {
-  "ank_userpass.age".publicKeys = [ank];
-  "tsauthkey.age".publicKeys = [ank karkinos];
+  oppy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINCW3CZ4r7VhI7+4rC+oOE4n3AMXEy3F2vm8jjHeTClR";
+  systems = [
+    karkinos
+    oppy
+  ];
+in
+{
+  "ank_userpass.age".publicKeys = [ ank ];
+  "tsauthkey.age".publicKeys = users ++ systems;
 }
