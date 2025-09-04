62641541eead4dbdeb10d7ad46328b39bc03e26b
Ankur Kumar <ank@leoank.me>
2025-09-04 17:30:57 -0400

    chore(hmm: ank): add atuin

--- homes/ank/home.nix
+++ homes/ank/home.nix
@@ -5,5 +5,15 @@
     homeDirectory = "/home/ank";
 
     packages = import ./packages.nix { inherit pkgs outputs; };
+
+    programs.atuin = {
+      enable = true;
+      settings = {
+        auto_sync = true;
+        sync_frequency = "1m";
+        sync_address = "https://api.atuin.sh";
+        search_mode = "fuzzy";
+      };
+    };
   };
 }
