4af96787085557c7a10bb76f128f3381ab9dfe0b
Ankur Kumar <ank@leoank.me>
2025-09-04 17:57:34 -0400

    chore(hm: ank): add atuin

--- homes/ank/home.nix
+++ homes/ank/home.nix
@@ -6,14 +6,5 @@
 
     packages = import ./packages.nix { inherit pkgs outputs; };
 
-    programs.atuin = {
-      enable = true;
-      settings = {
-        auto_sync = true;
-        sync_frequency = "1m";
-        sync_address = "https://api.atuin.sh";
-        search_mode = "fuzzy";
-      };
-    };
   };
 }
--- homes/common/dev/terminals.nix
+++ homes/common/dev/terminals.nix
@@ -9,6 +9,16 @@
     (pkgs.writers.writePython3Bin "gclb" { } ./gclb.py)
   ];
   programs = {
+    atuin = {
+      enable = true;
+      enableZshIntegration = true;
+      settings = {
+        auto_sync = true;
+        sync_frequency = "5m";
+        sync_address = "https://api.atuin.sh";
+        search_mode = "fuzzy";
+      };
+    };
 
     wezterm = {
       enable = true;
