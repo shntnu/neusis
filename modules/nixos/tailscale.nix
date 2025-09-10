27cc3563210a0c5057083d5eae6aa0cb140232ad
Ankur Kumar <ank@leoank.me>
2025-09-10 13:25:22 -0400

    feat(nm: tailscale): add nixos tailscale module

---
+++ modules/nixos/tailscale.nix
@@ -0,0 +1,80 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+let
+  cfg = config.neusis.tailscale;
+in
+{
+  options = {
+    neusis.tailscale = {
+      enable = mkEnableOption "Enable Neusis tailscale config";
+
+      authkey_file = mkOption {
+        type = types.str;
+        default = null;
+        description = "Path to the tsauthkey file.";
+      };
+
+      persistent_authkey_file = mkOption {
+        type = types.path;
+        default = ../../secrets/common/persistent_tsauthkey.age;
+        description = "Path to the persistent tsauthkey file.";
+      };
+
+      ephemeral_authkey_file = mkOption {
+        type = types.path;
+        default = ../../secrets/common/ephemeral_tsauthkey.age;
+        description = "Path to the ephemeral tsauthkey file.";
+      };
+
+      isPersistent = mkOption {
+        type = types.bool;
+        default = false;
+        description = "Use userspace networking.";
+      };
+
+      isUserSpace = mkOption {
+        type = types.bool;
+        default = false;
+        description = "Use userspace networking.";
+      };
+
+      hostName = mkOption {
+        type = types.str;
+        default = null;
+        description = "Custom hostname to use.";
+      };
+
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+
+    # make the tailscale command usable to users
+    environment.systemPackages = [ pkgs.tailscale ];
+
+    # add agenix auth key
+    age.secrets.tsauthkey.file =
+      if cfg.authkey_file != null then
+        cfg.authkey_file
+      else if cfg.isPersistent then
+        cfg.persistent_authkey_file
+      else
+        cfg.ephemeral_authkey_file;
+
+    # enable the tailscale service
+    services.tailscale = {
+      enable = true;
+      authKeyFile = config.age.secrets.tsauthkey.path;
+      extraUpFlags = mkIf (cfg.hostName != null) [
+        "--hostname"
+        "${cfg.hostName}"
+      ];
+    };
+
+  };
+}
