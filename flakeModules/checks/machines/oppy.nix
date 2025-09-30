b95abcdc8380765c6192cb9b2522b53f87432fb5
Ankur Kumar <ank@leoank.me>
2025-09-29 21:04:59 -0400

    fix(oppy): add test
    This passes but tailscale still fails but that is expected. We can fix that later.

--- flakeModules/checks/machines/oppy.nix
+++ flakeModules/checks/machines/oppy.nix
@@ -16,15 +16,15 @@
     }:
     {
       checks = {
-        # FIXME: This fails because of creds issue
-        # oppy_test = pkgs.testers.runNixOSTest {
-        #   name = "oppy-test";
-        #   nodes.oppy.imports = self.nixosConfigurations.oppy._module.args.modules;
-        #   node.specialArgs = { inherit (self) inputs outputs; };
-        #   node.pkgsReadOnly = false;
-        #
-        #   testScript = builtins.readFile ./oppy_test.py;
-        # };
+        # FIXME: This passes but find a way to add creds for tailscale to work
+        oppy_test = pkgs.testers.runNixOSTest {
+          name = "oppy-test";
+          nodes.oppy.imports = self.nixosConfigurations.oppy._module.args.modules;
+          node.specialArgs = { inherit (self) inputs outputs; };
+          node.pkgsReadOnly = false;
+
+          testScript = builtins.readFile ./oppy_test.py;
+        };
       };
     };
 }
