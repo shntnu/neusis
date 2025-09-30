9bad24d82a74deb098faa463c2e5b2a2e31c6da1
Ankur Kumar <ank@leoank.me>
2025-09-29 20:33:14 -0400

    fix(oppy): remove login test

--- flakeModules/checks/machines/oppy_test.py
+++ flakeModules/checks/machines/oppy_test.py
@@ -4,14 +4,3 @@
 # ruff: noqa
 
 oppy.wait_for_unit("multi-user.target")
-
-with subtest("Log in as ank on a virtual console"):
-    oppy.wait_until_tty_matches("1", "login: ")
-    oppy.send_chars("ank\n")
-    oppy.wait_until_tty_matches("1", "login: ank")
-    oppy.wait_until_succeeds("pgrep login")
-    oppy.wait_until_tty_matches("1", "Password: ")
-    oppy.send_chars("changeme\n")
-    oppy.wait_until_succeeds("pgrep -u ank zsh")
-    oppy.send_chars("touch done\n")
-    oppy.wait_for_file("/home/ank/done")
