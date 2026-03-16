722045ee6d1c3aaaf730947dfd52cdcd2fbb1d3f
Alán F. Muñoz <amunozgo@broadinstitute.org>
2026-03-16 15:18:31 -0400

    feat(home/amunoz): add atuin and gemini-cli, simplify oppy modules (#5)
    * feat(home/amunoz): Add atuin and nom
    
    Add two new command-line tools:
    
    - atuin: For a magical shell history with syncing nom: A more
    - informative wrapper around `nix develop`
    
    * chore(home): Remove nom package
    
    * feat(home): Add atuin for shell history sync
    
    Configure atuin to synchronize shell history across machines.
    
    It is set to auto-sync every 5 minutes and includes integrations for
    fish, bash, and nushell. The background daemon is enabled on Linux
    systems.
    
    * chore(home/amunoz): remove atuin duplication
    
    * feat(home): Add gemini-cli to packages

--- homes/amunoz/machines/oppy.nix
+++ homes/amunoz/machines/oppy.nix
@@ -8,14 +8,14 @@
   imports = [
     ../home.nix
     ../../common/home_manager.nix
-    ../../common/dev
-    ../../common/dev/kalam.nix
-    ../../common/themes
-    (import ../../common/dev/editors.nix {
-      inherit pkgs config inputs;
-      enableNvim = false;
-      enableAstro = false;
-    })
+    # ../../common/dev
+    # ../../common/dev/kalam.nix
+    # ../../common/themes
+    # (import ../../common/dev/editors.nix {
+    #   inherit pkgs config inputs;
+    #   enableNvim = false;
+    #   enableAstro = false;
+    # })
     (import ../../common/dev/git.nix {
       username = "Alán F. Muñoz";
       userEmail = "amunozgo@broadinstitute.org";
