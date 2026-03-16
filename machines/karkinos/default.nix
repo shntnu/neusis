c66721df325c32dacddadf10e49ecc7d0661bce7
Ankur Kumar <ank@leoank.me>
2026-03-16 11:04:15 -0400

    chore(karkinos: talinet): move to shantanu tailnet

--- machines/karkinos/default.nix
+++ machines/karkinos/default.nix
@@ -54,7 +54,11 @@
     isPersistent = true;
     hostName = "karkinos";
     forceHostName = true;
+    persistent_authkey_file = ../../secrets/common/persistent_cslab_mesh.age;
+    clientIdFile = ../../secrets/common/tsclient.age;
+    clientSecretFile = ../../secrets/common/tssecret.age;
     disableKeyExpiry = true;
+    tailnetOrg = "shntnu.github";
   };
 
   neusis.services.monitoring = {
---
+++ users/cslab_karkinos.nix
@@ -0,0 +1,72 @@
+{ pkgs, ... }:
+{
+  admins = [
+    {
+      username = "ank";
+      fullName = "Ankur Kumar";
+      shell = pkgs.zsh;
+      sshKeys = [
+        ../homes/ank/id_rsa.pub
+        ../homes/ank/id_ed25519.pub
+        ../homes/ank/id2_ed25519.pub
+      ];
+      homeModules = {
+        oppy = [
+          ../homes/ank/machines/oppy.nix
+        ];
+        karkinos = [
+          ../homes/ank/machines/karkinos.nix
+        ];
+      };
+    }
+    {
+      username = "amunoz";
+      fullName = "Alan";
+      shell = pkgs.fish;
+      sshKeys = [
+        ../homes/amunoz/id_ed25519.pub
+      ];
+      homeModules = {
+        karkinos = [
+          ../homes/amunoz/machines/oppy.nix
+        ];
+      };
+    }
+    {
+      username = "shsingh";
+      fullName = "Shantanu";
+      shell = pkgs.zsh;
+      sshKeys = [
+        ../homes/shsingh/id_ed25519.pub
+      ];
+      homeModules = {
+        karkinos = [
+          ../homes/shsingh/machines/oppy.nix
+        ];
+      };
+    }
+    {
+      username = "jfredinh";
+      fullName = "Johan";
+      shell = pkgs.zsh;
+      sshKeys = [
+        ../homes/jfredinh/id_ed25519.pub
+      ];
+      homeModules = {
+        karkinos = [
+          ../homes/jfredinh/machines/oppy.nix
+        ];
+      };
+    }
+  ];
+
+  regulars = [
+  ];
+
+  # Locked users - accounts exist but cannot login, data preserved
+  locked = [
+  ];
+
+  guests = [ ];
+
+}
