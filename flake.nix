19a238f3aa3c685a5bf199f608d3b166915b33b3
Alán F. Muñoz <amunozgo@broadinstitute.org>
2026-03-19 13:18:56 -0400

    chore(flake): Update nixpkgs to nixos-25.11
    Update the nixpkgs input to the nixos-25.11 branch and refresh the
    lock file to update all dependencies.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-master.url = "github:nixos/nixpkgs/master";
 
